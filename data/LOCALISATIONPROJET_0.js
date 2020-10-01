7df53edd61ecfe65f11e83531a5032fc5faaf23d
DOUAZIACHRAF <72155362+DOUAZIACHRAF@users.noreply.github.com>
2020-10-01 09:50:31 +0100

    Add files via upload

---
+++ data/LOCALISATIONPROJET_0.js
@@ -0,0 +1,12 @@
+var json_LOCALISATIONPROJET_0 = {
+"type": "FeatureCollection",
+"name": "LOCALISATIONPROJET_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "PROJET_ID": 1.0, "PROJET_NOM DU PROJET": "VELODROME", "PROJET_X": "33.589372", "PROJET_Y": "-7.645648", "PROJET_(X,Y)": "33.589372, -7.645648", "PROJET_LIEN DU PROJET": "https:\/\/douaziachraf.github.io\/geodataachraf\/" }, "geometry": { "type": "Point", "coordinates": [ -7.645598117225709, 33.58935049882502 ] } },
+{ "type": "Feature", "properties": { "id": "2", "PROJET_ID": 2.0, "PROJET_NOM DU PROJET": "DERB GHELEF", "PROJET_X": "33.574226", "PROJET_Y": "-7.628855", "PROJET_(X,Y)": "33.574226, -7.628855", "PROJET_LIEN DU PROJET": "https:\/\/douaziachraf.github.io\/geodataachraf\/" }, "geometry": { "type": "Point", "coordinates": [ -7.628710309369078, 33.574079960376693 ] } },
+{ "type": "Feature", "properties": { "id": "3", "PROJET_ID": 3.0, "PROJET_NOM DU PROJET": "DALLAS", "PROJET_X": "33.556685", "PROJET_Y": " -7.689129", "PROJET_(X,Y)": "33.556685, -7.689129", "PROJET_LIEN DU PROJET": "https:\/\/douaziachraf.github.io\/geodataachraf\/" }, "geometry": { "type": "Point", "coordinates": [ -7.688893078138266, 33.556893986141709 ] } },
+{ "type": "Feature", "properties": { "id": "4", "PROJET_ID": 4.0, "PROJET_NOM DU PROJET": "COTEVILLA", "PROJET_X": "33.531229", "PROJET_Y": " -7.648798", "PROJET_(X,Y)": "33.531229, -7.648798", "PROJET_LIEN DU PROJET": "https:\/\/douaziachraf.github.io\/geodataachraf\/" }, "geometry": { "type": "Point", "coordinates": [ -7.648931196261096, 33.531225767056583 ] } },
+{ "type": "Feature", "properties": { "id": "5", "PROJET_ID": 5.0, "PROJET_NOM DU PROJET": "BOUSKOURA", "PROJET_X": "33.464265", "PROJET_Y": "-7.655350", "PROJET_(X,Y)": "33.464265, -7.655350", "PROJET_LIEN DU PROJET": "https:\/\/douaziachraf.github.io\/geodataachraf\/" }, "geometry": { "type": "Point", "coordinates": [ -7.655492262840505, 33.464422934022345 ] } }
+]
+}
